cde801058d0acce09d78245bbcacf6f942afe5eb
Tiffany Zheng <tiz90@pitt.edu>
2025-11-10 22:50:05 -0500

    commit

--- Problems/problem09/Image.cpp
+++ Problems/problem09/Image.cpp
@@ -7,26 +7,54 @@ Image::Image() : width(0), height(0), image_data(nullptr) {}
 Image::Image(int size)
 {
     // TODO
+    width = size;
+    height = size;
+    // Square image of size x size are the same as the Rectangle
+    image_data = new RGBPIXEL[width * height];
+    for (int i = 0; i < width * height; i++)
+    {
+        image_data[i] = GREEN; // All pixels should be set to Green
+    }
 }
 
 Image::Image(int w, int h)
 {
     // TODO
+    // Rectangle image of width w and height h have the same equation as the Square image
+    width = w;
+    height = h;
+    image_data = new RGBPIXEL[width * height]; // So this is using the same logic
+    for (int i = 0; i < width * height; i++)
+    {
+        image_data[i] = GREEN; // All pixels are set to Green
+    }
 }
-
+// Rule of 3 - Copy Constructor
 Image::Image(const Image &im)
 {
     // TODO: make sure to apply deep copy
+    width = im.width;
+    height = im.height;
+    image_data = new RGBPIXEL[width * height];
+    for (int i = 0; i < width * height; i++)
+    {
+        image_data[i] = im.image_data[i];
+    }
 }
-
+// Rule of 3 - Destructor
 Image::~Image()
 {
     // TODO
+    delete[] image_data;
 }
-
+// Rule of 3 - Copy Assignment
 Image &Image::operator=(Image im)
 {
     // TODO: make sure to apply copy-swap-idiom
+    width = im.width; // Passing values
+    height = im.height;
+    std::swap(image_data, im.image_data); // Swapping pointers
+    return *this;
 }
 
 int Image::get_width() const
@@ -71,12 +99,20 @@ bool Image::save_to_file(std::string filename)
 
 Image operator+(const Image &im, const RGBPIXEL &val)
 {
-    // TODO
+    // TODO - Based on SafeArray5
+    Image result(im.get_width(), im.get_height());
+    // element-wise adding val
+    for (int i = 0; i < im.get_width() * im.get_height(); i++)
+    {
+        result.set_pixel(i, im.get_pixel(i) + val);
+    }
+    return result;
 }
 
 Image operator+(const RGBPIXEL &val, const Image &im)
 {
     // TODO
+    return operator+(val, im);
 }
 
 // This will be useful if attempting bonus
