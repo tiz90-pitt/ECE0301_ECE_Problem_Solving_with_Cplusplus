61b739af1debc35d717fdde9ee3eb64a648c07a2
Tiffany Zheng <tiz90@pitt.edu>
2025-11-12 04:20:13 -0500

    commit

--- Problems/exam2/Exam2.hpp
+++ Problems/exam2/Exam2.hpp
@@ -20,27 +20,27 @@ public:
   void displayMessage() const;
   // ...
 };
-class chip{
-private:
-double res;
-int volA, volB;
-std::string name;
-public:
-chip(){res=0; volA=0; volB=0;}
-chip(double r, int va, int vb){res=r; volA=va; volB=vb;}
+// class chip{
+// private:
+// double res;
+// int volA, volB;
+// std::string name;
+// public:
+// chip(){res=0; volA=0; volB=0;}
+// chip(double r, int va, int vb){res=r; volA=va; volB=vb;}
 
-void set_res(double r) {res=r;};
-void set_chip_name(std::string n) {name=n;};
+// void set_res(double r) {res=r;};
+// void set_chip_name(std::string n) {name=n;};
 
-double get_res() const {return res;};
-double get_vol_diff() const {return volA-volB;};
+// double get_res() const {return res;};
+// double get_vol_diff() const {return volA-volB;};
 
-virtual double get_current() const {return 0;};
-};
-class chip_74 : public chip{
-  public:
-    using ::chip::chip;
-    double get_current() const {return get_vol_diff() / get_res();
-  }
-};
+// virtual double get_current() const {return 0;};
+// };
+// class chip_74 : public chip{
+//   public:
+//     using ::chip::chip;
+//     double get_current() const {return get_vol_diff() / get_res();
+//   }
+// };
 #endif
